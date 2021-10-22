c187b5ca35aac74506108b89b9ed0b461fe581a7
ichiniro <gioelgio55@gmail.com>
2021-10-22 18:20:35 -0500

    Gtk+ y WinTest

--- src/sonartest.sh
+++ src/sonartest.sh
@@ -4,5 +4,5 @@ ssh -L 8080:localhost:9000 ichi@192.168.122.99
 
 ./gradlew sonarqube \
   -Dsonar.projectKey=Duckish \
-  -Dsonar.host.url=http://localhost:8080 \
-  -Dsonar.login=a5728b158edeb54659356dc9d6dd579538004663
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=417d022ee7f66409b7f34b8ec5263b8991cc4c65
