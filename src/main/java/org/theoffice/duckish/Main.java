b6158db58b644eb1279f0da5c4c8541daa0ad91d
AtreyuLodbrok <coopervan6@gmail.com>
2021-11-21 01:09:10 -0600

    Frame System Principal conect with kitchen

--- src/main/java/org/theoffice/duckish/Main.java
+++ src/main/java/org/theoffice/duckish/Main.java
@@ -6,16 +6,19 @@ import org.theoffice.duckish.proc.Start;
 
 import javax.swing.*;
 import java.awt.*;
+import org.theoffice.duckish.ui.SystemUI;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         // Config UI
-        new SetUI();
-        new SetUIFont(new javax.swing.plaf.FontUIResource("Roboto", Font.PLAIN, 14));
-
-        JFrame.setDefaultLookAndFeelDecorated(true); // Doesn't work IDK why
-        new Start();
+        //new SetUI();
+        //new SetUIFont(new javax.swing.plaf.FontUIResource("Roboto", Font.PLAIN, 14));
+        
+        //Initialize Principal Frame xd aiudaa
+        SystemUI inc = new SystemUI();
+        inc.setVisible(true);        
+        //new Start();
     }
 }
